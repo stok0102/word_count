c18bc1f4503193d1d8a48fa104830348c5e4fb79
Caleb Stokka <caleb.stokka@gmail.com>
2016-06-30 20:03:29 -0700

    passes first spec, false case

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,5 +1,13 @@
+require('pry')
+
 class String
   def word_count (user_input)
-
+    input_array = user_input.split(" ")
+    binding.pry
+    if input_array.include? self
+      1
+    else
+      0  
+    end
   end
 end
