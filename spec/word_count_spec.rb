4f33285899baa08817a618aa891f0f9e3cfb73f9
Caleb Stokka <caleb.stokka@gmail.com>
2016-06-30 20:04:29 -0700

    passes first passing spec

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -5,4 +5,7 @@ describe('String#word_count') do
   it('takes a word and a string and displays a count of the number of times that word shows up' )do
     expect('jack'.word_count('jill')).to(eq(0))
   end
+  it('takes a word and a string and displays a count of the number of times that word shows up' )do
+    expect('jack'.word_count('jack')).to(eq(1))
+  end
 end
