9f292179f5be691c2c46e1e0487947c696eb9445
Caleb Stokka <caleb.stokka@gmail.com>
2016-07-01 09:35:46 -0700

    update unit testing specs

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -2,16 +2,16 @@ require('rspec')
 require('word_count')
 
 describe('String#word_count') do
-  it('takes a word and another word and displays a count of the number of times that word shows up' )do
+  it('will return 0 if given non matching words' )do
     expect('jack'.word_count('jill')).to(eq(0))
   end
-  it('takes a word and the same word and displays a count of the number of times that word shows up' )do
+  it('will return 1 if given two matching words' )do
     expect('jack'.word_count('jack')).to(eq(1))
   end
-  it('takes a word and a string of words and displays a count of the number of times that word shows up' )do
+  it('will return 1 if given a word and a string containing that word' )do
     expect('jack'.word_count('jack jill')).to(eq(1))
   end
-  it('takes a word and a string of words and displays a count of the number of times that word shows up' )do
+  it('will return 2 if given a word and a string containing 2 instances of that word' )do
     expect('peck'.word_count("If Peter Piper picked a peck of pickled peppers, where's the peck of pickled peppers Peter Piper picked?")).to(eq(2))
   end
 end
