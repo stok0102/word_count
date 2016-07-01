0b6bd0034b51c65506fdeb3bb762f251f7f451b7
Caleb Stokka <caleb.stokka@gmail.com>
2016-07-01 09:42:01 -0700

    update integration spec

--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -6,7 +6,7 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('the word count path', {:type => :feature}) do
-  it('should gather user input and direct to a page with the resulting number') do
+  it('gathers user word and string and returns number of instances of word in string') do
     visit('/')
     fill_in('user_word', :with => 'jack')
     fill_in('user_string', :with => 'jack and jill went up the hill to fetch a pail of water. jack fell down and broke his crown and jill came tumbling after')
